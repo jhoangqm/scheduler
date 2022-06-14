937320db96d3e69952049b37b8e1cec65c0568db
jhoangqm <jimmyhoangmtl@gmail.com>
2022-06-14 10:06:27 -0400

    Completes DayList component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,19 +2,19 @@ import React from 'react';
 import DayListItem from './DayListItem';
 
 export default function DayList(props) {
-  const mapped = props.days.map((eachDay) => {
+  const mappedDays = props.days.map((eachDay) => {
     return (
       <ul>
-        <DayListItem>
+        <DayListItem
           key={eachDay.id}
           name={eachDay.name}
           spots={eachDay.spots}
           selected={eachDay.name === props.day}
           setDay={props.setDay}
-        </DayListItem>
+        />
       </ul>
     );
   });
 
-  return <ul>{mapped}</ul>;
+  return <ul>{mappedDays}</ul>;
 }
