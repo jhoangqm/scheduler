16926c9eb094673a60082c92025d4173361ed1fe
jhoangqm <jimmyhoangmtl@gmail.com>
2022-06-14 20:20:01 -0400

    Display appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,36 @@
 import './styles.scss';
 
-import React from 'react';
+import React, { Fragment } from 'react';
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
+// import Form from './Form';
+// import Confirm from './Confirm';
+// import Status from './Status';
+// import Error from './Error';
 
-function Appointment() {
-  return <article className="appointment"></article>;
+function Appointment(props) {
+  return (
+    <Fragment>
+      <Header time={props.time} />
+      {props.interview ? (
+        <>
+          <article className="appointment">
+            <Show
+              student={props.interview.student}
+              interviewer={props.interview.interviewer}
+            />
+          </article>
+        </>
+      ) : (
+        <>
+          <article className="appointment">
+            <Empty />
+          </article>
+        </>
+      )}
+    </Fragment>
+  );
 }
 
 export default Appointment;
