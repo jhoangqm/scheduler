fcbb61eaad46ea26f7e88d0959058e29f80f2f81
jhoangqm <jimmyhoangmtl@gmail.com>
2022-06-15 17:05:14 -0400

    Managing states

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -4,58 +4,69 @@ import axios from 'axios';
 import 'components/Application.scss';
 import Appointment from './Appointment';
 import DayList from './DayList';
+import { getAppointmentsForDay } from "helpers/selectors";
 
 const appointments = {
-  1: {
+  "1": {
     id: 1,
-    time: '12pm',
+    time: "12pm",
   },
-  2: {
+  "2": {
     id: 2,
-    time: '1pm',
+    time: "1pm",
     interview: {
-      student: 'Lydia Miller-Jones',
-      interviewer: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
         id: 3,
-        name: 'Sylvia Palmer',
-        avatar: 'https://i.imgur.com/LpaY82x.png',
-      },
-    },
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
   },
-  3: {
+  "3": {
     id: 3,
-    time: '2pm',
+    time: "2pm",
   },
-  4: {
+  "4": {
     id: 4,
-    time: '3pm',
+    time: "3pm",
     interview: {
-      student: 'Archie Andrews',
-      interviewer: {
+      student: "Archie Andrews",
+      interviewer:{
         id: 4,
-        name: 'Cohana Roy',
-        avatar: 'https://i.imgur.com/FK8V841.jpg',
-      },
-    },
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
   },
-  5: {
+  "5": {
     id: 5,
-    time: '4pm',
-  },
+    time: "4pm",
+  }
 };
 
 
-
 export default function Application() {
-  const [day, setDay] = useState([]);
+// const [day, setDay] = useState("Monday");
+// const [days, setDays] = useState([]);
+
+  const [state, setState] = useState({
+    day: "Monday", // setDay function handles this
+    days: [],      // setDays function handles this
+    appointments: {}
+  });
+
+  // Aliasing creates a function called setDay that updates the day state
+  const setDay = day => setState({ ...state, day });
+  // Aliasing creates a function called setDay that updates the days state
+  const setDays = days => setState(prev => ({...prev, days}));
 
   const parsedAppointment = Object.values(appointments).map((appointment) => {
     return <Appointment key={appointment.id} {...appointment} />;
   });
 
   useEffect(()=>{
-    const url = `http://localhost:8001/api/days`
-    axios.get(url).then(response => {setDay([...response.data])});
+    axios.get("/api/days").then(response => setDays(response.data));
   },[])
 
   return (
@@ -68,7 +79,7 @@ export default function Application() {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={day} value={day} onChange={setDay} />
+          <DayList days={state.days} value={state.day} onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,19 +1,19 @@
 export function getAppointmentsForDay(state, day) {
   const { days, appointments } = state; // Adds readability to make it easier to understand
   const filteredDay = days.filter((eachDay) => eachDay.name === day);
-  console.log('filteredDay :', filteredDay);
+  // console.log('filteredDay :', filteredDay);
 
   if (filteredDay.length === 0 || days.length === 0) {
     return [];
   }
 
   const appointmentsArray = filteredDay[0].appointments;
-  console.log('AppointmentsArray :', appointmentsArray);
+  // console.log('AppointmentsArray :', appointmentsArray);
 
   const parsedAppointments = appointmentsArray.map((appointmentID) => {
-    console.log('appointments : ', appointments[appointmentID]);
+    // console.log('appointments : ', appointments[appointmentID]);
     return appointments[appointmentID];
   });
-  console.log(parsedAppointments);
+  // console.log(parsedAppointments);
   return parsedAppointments;
 }
