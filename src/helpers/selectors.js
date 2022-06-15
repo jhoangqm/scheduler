e52515776037295ea32ed8b0f52ef37ae6ffbdf7
jhoangqm <jimmyhoangmtl@gmail.com>
2022-06-15 16:10:42 -0400

    Destructures parameter state for better readability

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,17 +1,18 @@
 export function getAppointmentsForDay(state, day) {
-  const filteredDay = state.days.filter((eachDay) => eachDay.name === day);
-  // console.log('filteredDay :', filteredDay);
+  const { days, appointments } = state; // Adds readability to make it easier to understand
+  const filteredDay = days.filter((eachDay) => eachDay.name === day);
+  console.log('filteredDay :', filteredDay);
 
-  if (filteredDay.length === 0 || state.days.length === 0) {
+  if (filteredDay.length === 0 || days.length === 0) {
     return [];
   }
 
   const appointmentsArray = filteredDay[0].appointments;
-  // console.log('AppointmentsArray :', appointmentsArray);
+  console.log('AppointmentsArray :', appointmentsArray);
 
   const parsedAppointments = appointmentsArray.map((appointmentID) => {
-    // console.log('appointments : ', state.appointments[appointmentID]);
-    return state.appointments[appointmentID];
+    console.log('appointments : ', appointments[appointmentID]);
+    return appointments[appointmentID];
   });
   console.log(parsedAppointments);
   return parsedAppointments;
