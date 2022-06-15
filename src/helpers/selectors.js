a3936a017d7ae28a2086a0cba62474d12fb160ee
jhoangqm <jimmyhoangmtl@gmail.com>
2022-06-15 16:00:48 -0400

    Completes helper function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,1 +1,18 @@
-export function getAppointmentsForDay(state, day) {}
+export function getAppointmentsForDay(state, day) {
+  const filteredDay = state.days.filter((eachDay) => eachDay.name === day);
+  // console.log('filteredDay :', filteredDay);
+
+  if (filteredDay.length === 0 || state.days.length === 0) {
+    return [];
+  }
+
+  const appointmentsArray = filteredDay[0].appointments;
+  // console.log('AppointmentsArray :', appointmentsArray);
+
+  const parsedAppointments = appointmentsArray.map((appointmentID) => {
+    // console.log('appointments : ', state.appointments[appointmentID]);
+    return state.appointments[appointmentID];
+  });
+  console.log(parsedAppointments);
+  return parsedAppointments;
+}
