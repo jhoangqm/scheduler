d96862c115c2f2bf83280e5815bdb6f8494f155f
jhoangqm <jimmyhoangmtl@gmail.com>
2022-06-16 13:01:32 -0400

    Adds transition and back function inside custom hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,6 +2,18 @@ import { React, useEffect, useState } from 'react';
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  return { mode };
+  function transition(newMode) {
+    setMode(newMode);
+  }
+
+  function back() {
+    console.log(history);
+    if (history.length > 1) {
+      setMode(history[history.length - 1]);
+    }
+  }
+
+  return { mode, transition, back };
 }
