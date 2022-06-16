fbdb33a3b64a57de26b58250306f4d2276781305
jhoangqm <jimmyhoangmtl@gmail.com>
2022-06-16 13:20:21 -0400

    Completes useVisualMode custom hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,13 +4,23 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition(newMode) {
+  function transition(newMode, replace) {
+    if (replace) {
+      // if replace is truthy, remove the last element of the array history
+      history.pop();
+      // changes the state with setHistory and creates a shallow copy of history and adds, newMode to the array
+      setHistory([...history, newMode]);
+    } else {
+      setHistory([...history, newMode]);
+    }
     setMode(newMode);
   }
 
   function back() {
-    console.log(history);
     if (history.length > 1) {
+      // returns the element that was removed if history array lenght is bigger than 1
+      history.pop();
+      // this will remove one length from the array
       setMode(history[history.length - 1]);
     }
   }
