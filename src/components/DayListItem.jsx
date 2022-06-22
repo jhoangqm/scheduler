1f9f3c21a27a4edc33663879699896e7503509d0
jhoangqm <jimmyhoangmtl@gmail.com>
2022-06-22 16:42:41 -0400

    Adds E2E cypress testing

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -23,7 +23,7 @@ export default function DayListItem(props) {
   });
 
   return (
-    <li onClick={setDay} className={dayClass}>
+    <li onClick={setDay} className={dayClass} selected={selected} data-testid="day">
       <h2 className="text--regular">{name}</h2>
       <h3 className="text--light">{formatSpots(spots)}</h3>
     </li>
